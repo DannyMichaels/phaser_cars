d2b682b64a9b154acf52d5abda3d497384c9f919
Daniel <itzdanielmichael@gmail.com>
2021-09-27 13:42:53 -0400

    update align.js

--- js/classes/util/align.js
+++ js/classes/util/align.js
@@ -8,4 +8,12 @@ class Align {
     obj.x = game.config.width / 2;
     obj.y = game.config.height / 2;
   }
+
+  static centerHorizontally(obj) {
+    obj.x = game.config.width / 2;
+  }
+
+  static centerVertically(obj) {
+    obj.y = game.config.height / 2;
+  }
 }
